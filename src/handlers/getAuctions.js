2314b95a4cdb9261119e229506de06632f28d098
Andrew Cathcart <andrew.cathcart@salecycle.com>
2020-07-12 13:05:12 +0100

    getAuction validation

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -1,17 +1,29 @@
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
   let auctions;
 
-  try {
-    const result = await dynamodb
-      .scan({ TableName: process.env.AUCTIONS_TABLE_NAME })
-      .promise();
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status,
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
 
+  try {
+    const result = await dynamodb.query(params).promise();
     auctions = result.Items;
   } catch (error) {
     console.log(error);
@@ -24,4 +36,9 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions).use(
+  validator({
+    inputSchema: getAuctionsSchema,
+    useDefaults: true,
+  }),
+);
